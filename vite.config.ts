96a170e5da75d53791353f8bd7ee3951f11d6514
Yang Di <young@deelol.com>
2022-08-01 18:21:50 +0800

    save config

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
@@ -7,7 +7,6 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig(({mode}) => {
-  const env = loadEnv(mode, __dirname)
   return {
     plugins: [
       vue(),
@@ -26,6 +25,5 @@ export default defineConfig(({mode}) => {
         "@": path.resolve(__dirname, "src"),
       },
     },
-    base: env.API_URL
   }
 })
