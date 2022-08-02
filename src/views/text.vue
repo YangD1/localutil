afcde5be9e3ed1cf00bb0b579c92da42c196d21e
Yang Di <young@deelol.com>
2022-08-02 10:57:42 +0800

    添加text文字存储功能

--- src/views/text.vue
+++ src/views/text.vue
@@ -1,5 +1,16 @@
 <template>
-text
+  <div style="margin: 20px 0" />
+  <el-input v-model="textarea" :autosize="{ minRows: 2, maxRows: 4 }" type="textarea" placeholder="Please input" />
+  <el-button type="primary" style="margin-top: 10px" @click="submit">提交</el-button>
 </template>
-<script setup>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+import axios from 'axios'
+const textarea = ref('')
+const submit = (): void => {
+  axios.post(textarea.value).then((res): void => {
+    console.log(res)
+  })
+}
 </script>
--- src/views/upload.vue
+++ src/views/upload.vue
@@ -1,5 +1,68 @@
 <template>
-upload
+  <el-upload
+    v-model:file-list="fileList"
+    class="upload-demo"
+    :action="`${API}localutil/upload`"
+    multiple
+    :on-preview="handlePreview"
+    :on-remove="handleRemove"
+    :before-remove="beforeRemove"
+    :limit="3"
+    :on-exceed="handleExceed"
+  >
+    <el-button type="primary">Click to upload</el-button>
+    <template #tip>
+      <div class="el-upload__tip">
+        点击进行文件上传
+      </div>
+    </template>
+  </el-upload>
 </template>
-<script setup>
+<script lang="ts" setup>
+import { ref, onMounted } from 'vue'
+import { ElMessage, ElMessageBox } from 'element-plus'
+
+import type { UploadProps, UploadUserFile } from 'element-plus'
+
+const API = ref(import.meta.env.VITE_BASE_API)
+onMounted(() => {
+  console.log(API.value)
+  
+})
+
+const fileList = ref<UploadUserFile[]>([
+  // {
+  //   name: 'element-plus-logo.svg',
+  //   url: 'https://element-plus.org/images/element-plus-logo.svg',
+  // },
+  // {
+  //   name: 'element-plus-logo2.svg',
+  //   url: 'https://element-plus.org/images/element-plus-logo.svg',
+  // },
+])
+
+const handleRemove: UploadProps['onRemove'] = (file, uploadFiles) => {
+  console.log(file, uploadFiles)
+}
+
+const handlePreview: UploadProps['onPreview'] = (uploadFile) => {
+  console.log(uploadFile)
+}
+
+const handleExceed: UploadProps['onExceed'] = (files, uploadFiles) => {
+  ElMessage.warning(
+    `The limit is 3, you selected ${files.length} files this time, add up to ${
+      files.length + uploadFiles.length
+    } totally`
+  )
+}
+
+const beforeRemove: UploadProps['beforeRemove'] = (uploadFile, uploadFiles) => {
+  return ElMessageBox.confirm(
+    `Cancel the transfert of ${uploadFile.name} ?`
+  ).then(
+    () => true,
+    () => false
+  )
+}
 </script>
